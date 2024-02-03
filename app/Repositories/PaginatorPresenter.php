ff4a6629e791d072a6a122656bdbfeaaf675bbe0
marcosdealmeidanascimento <marcos.800nascimento@gmail.com>
2024-02-03 06:50:56 -0300

    feat: Pagination and Presenter

---
+++ app/Repositories/PaginatorPresenter.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Repositories;
+
+
+use Illuminate\Pagination\LengthAwarePaginator;
+use stdClass;
+
+class PaginatorPresenter implements PaginationInterface
+{
+
+    /**
+     * @var stdClass[]
+     */
+
+    private array $items;
+
+    public function __construct(
+        protected LengthAwarePaginator $paginator
+    ) {
+        $this->items = $this->resolveItems($this->paginator->items());
+    }
+
+    public function items(): array
+    {
+        return $this->items;
+    }
+    public function total(): int
+    {
+        return $this->paginator->total() ?? 0;
+    }
+    public function isFirstPage(): bool
+    {
+        return $this->paginator->onFirstPage();
+    }
+    public function isLastPage(): bool
+    {
+        return $this->paginator->hasMorePages();
+    }
+    public function currentPage(): int
+    {
+        return $this->paginator->currentPage() ?? 1;
+    }
+    public function getNextPageNumber(): int
+    {
+        return $this->paginator->currentPage() + 1;
+    }
+    public function getPreviousPageNumber(): int
+    {
+        return $this->paginator->currentPage() - 1;
+    }
+
+    private function resolveItems(array $items): array
+    {
+        $response = [];
+        foreach ($items as $key => $item) {
+            $stdClassObject = new stdClass;
+            foreach ($item->toArray() as $key => $value) {
+                $stdClassObject->$key = $value;
+            }
+            array_push($response, $stdClassObject);
+        }
+        return $response;
+    }
+}
